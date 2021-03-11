e5bf093419f53d33a9b18f10435cd4ac7cb6fc72
ye-haowen <18397946275@163.com>
2021-03-11 17:24:36 +0800

    去除单据模块

--- src/router/index.ts
+++ src/router/index.ts
@@ -116,17 +116,17 @@ const routes: RouteConfig[] = [
         component: () => import('../views/store/detail.vue')
       }
     ]
-  }, {
-    path: '/document',
-    name: '单据',
-    component: () => import('@/views/documents/index.vue'),
-    children: [
-      {
-        path: 'GDFE',
-        name: '出口货物报关单',
-        component: () => import('@/views/documents/GDFE.vue')
-      }
-    ]
+    // }, {
+    //   path: '/document',
+    //   name: '单据',
+    //   component: () => import('@/views/documents/index.vue'),
+    //   children: [
+    //     {
+    //       path: 'GDFE',
+    //       name: '出口货物报关单',
+    //       component: () => import('@/views/documents/GDFE.vue')
+    //     }
+    //   ]
   }
 ]
 
